b4ab46c78406174231951a16c05f2e28a430887e
Ryan Pavlik <ryan.pavlik@collabora.com>
2019-09-11 12:03:23 -0500

    specify clang-format 6 for hpp

--- generate-openxr-hpp.sh
+++ generate-openxr-hpp.sh
@@ -3,6 +3,6 @@ set -e
 (
 cd $(dirname $0)
 src/scripts/src_genxr.py -registry specification/registry/xr.xml -o include/openxr openxr.hpp
-clang-format -style=file -i include/openxr/openxr.hpp
+clang-format-6.0 -style=file -i include/openxr/openxr.hpp
 
 )
