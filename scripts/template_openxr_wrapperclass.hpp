7c5aa5f9b9f7f66237b1f9daa723db84ae15814b
Ryan Pavlik <ryan.pavlik@collabora.com>
2019-09-17 13:19:17 -0500

    Expand the generic wrapper template to prepare for making handles use it too.

--- scripts/template_openxr_wrapperclass.hpp
+++ scripts/template_openxr_wrapperclass.hpp
@@ -6,47 +6,91 @@
 //#     set input_param_type = type
 //# endif
 
+//## Default to only providing equality/inequality
+//# if not comparison_operators
+//#     set comparison_operators = ('==', '!=')
+//# endif
+
+//# block comment_class
 //! /*{ raw_type }*/ wrapper class
 //!
 //! See the related specification text at /*{ make_spec_url(type) }*/
+//# endblock
 class /*{ type }*/ {
    public:
-    //# block constructor_default
+
+//# block member_types
+//# endblock
+
+    /*!
+     * @name Constructors, assignment, and conversions
+     * @{
+     */
+//# block constructor_default
     //! Default constructor.
     OPENXR_HPP_CONSTEXPR /*{ type }*/ () = default;
-    //# endblock
+//# endblock
 
-    //# block constructor_explicit
+//# block constructor_explicit
     //! Explicit constructor from raw /*{ raw_type }*/
     OPENXR_HPP_CONSTEXPR explicit /*{ type }*/ (/*{ raw_type }*/ v) noexcept : val_(v) {}
-    //# endblock
+//# endblock
 
-    //# block conversion_explicit_bool
+//# block extra_constructors_conversion_assign
+//# endblock
+
+    //! @}
+
+    /*!
+     * @name Validity checking
+     * @{
+     */
+//# block conversion_explicit_bool
     //# if invalid
-    //! True if this /*{ type }*/ is valid
+    //! Returns true in conditionals if this /*{ type }*/ is valid
     OPENXR_HPP_CONSTEXPR explicit operator bool() const noexcept { return val_ != /*{ invalid }*/; }
     //# endif
-    //# endblock
+//# endblock
 
-    //# block method_get
+//# block operator_negation
+    //# if invalid
+    //! Unary negation: True if this /*{ type }*/ is invalid
+    OPENXR_HPP_CONSTEXPR bool operator!() const noexcept { return val_ == /*{ invalid }*/; }
+    //# endif
+//# endblock
+
+//# block extra_validity
+//# endblock
+    //! @}
+
+    //! @name Raw /*{ raw_type }*/ manipulation
+    //! @{
+
+//# block method_get
     //! Gets the raw /*{ raw_type }*/ type.
     OPENXR_HPP_CONSTEXPR /*{ raw_type }*/ get() const noexcept { return val_; }
-    //# endblock
+//# endblock
 
-    //# block method_put
+//# block method_put
     //! @brief Clears this value, then returns the address of the raw /*{ raw_type }*/ type,
     //! for use in creation/assignment.
     /*{ raw_type }*/ *put() noexcept {
         val_ = /*{ invalid if invalid else "0"}*/;
         return &val_;
     }
-    //# endblock
+//# endblock
+    //! @}
 
-    /*% block extra_methods %*/ /*% endblock %*/
+//# block extra_methods
+//# endblock
 
    private:
     /*{ raw_type }*/ val_{/*{ invalid }*/};
 };
+
+//# block size_assert
+static_assert(sizeof(/*{ type }*/) == sizeof(/*{raw_type}*/), "raw type and wrapper have different size!");
+//# endblock
 
 //# block free_get
 //! @brief Free function for getting the raw /*{ raw_type }*/ from /*{type}*/.
@@ -54,22 +98,20 @@ class /*{ type }*/ {
 //! Should be found via argument-dependent lookup and thus not need explicit namespace qualification.
 //! @see /*{type}*/::get()
 //! @relates /*{type}*/
-OPENXR_HPP_CONSTEXPR inline /*{ raw_type }*/ get(/*{input_param_type}*/ v) noexcept { return v.get(); }
+OPENXR_HPP_CONSTEXPR OPENXR_HPP_INLINE /*{ raw_type }*/ get(/*{input_param_type}*/ v) noexcept { return v.get(); }
 //# endblock
 
 //# block free_put
+//# block free_put_comment
 //! @brief Free function for clearing and getting the raw /*{ raw_type }*/ from /*{type}*/.
 //!
 //! Should be found via argument-dependent lookup and thus not need explicit namespace qualification.
 //! @see /*{type}*/::put()
 //! @relates /*{type}*/
-inline /*{ raw_type }*/ *put(/*{type}*/ &v) noexcept { return v.put(); }
+//# endblock
+OPENXR_HPP_INLINE /*{ raw_type }*/ *put(/*{type}*/ &v) noexcept { return v.put(); }
 //# endblock
 
-//## Default to only providing equality/inequality
-//# if not comparison_operators
-//#     set comparison_operators = ('==', '!=')
-//# endif
 
 //# block compare_self
 //# for op in comparison_operators
@@ -96,4 +138,5 @@ OPENXR_HPP_CONSTEXPR OPENXR_HPP_INLINE bool operator/*{- op -}*/(/*{raw_type}*/ 
 //# endfor
 //# endblock
 
-/*% block extra_free_functions %*/ /*% endblock %*/
+//# block extra_free_functions
+//# endblock
